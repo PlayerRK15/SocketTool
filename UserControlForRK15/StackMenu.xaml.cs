f40f07ff83819c9ed44066e1eece82e11489fffe
PlayerRK15 <rk15@qq.com>
2024-04-01 12:59:33 +0800

    #上传控件项目

---
+++ UserControlForRK15/StackMenu.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlForRK15
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class StackMenu : UserControl
+    {
+
+
+        public bool IsShowMenu
+        {
+            get { return (bool)GetValue(IsShowMenuProperty); }
+            set { SetValue(IsShowMenuProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsShowMenu.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsShowMenuProperty =
+            DependencyProperty.Register("IsShowMenu", typeof(bool), typeof(StackMenu), new PropertyMetadata(true));
+
+
+        public StackMenu()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var animatoin = new DoubleAnimation();
+
+
+            if (IsShowMenu)
+            {
+                IsShowMenu = false;
+                animatoin.Duration = new Duration(TimeSpan.FromSeconds(1));
+                animatoin.To = this.Width - 20;
+                MenuContent.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                IsShowMenu = true;
+                MenuContent.Visibility = Visibility.Visible;
+            }
+            if (animatoin != null)
+            {
+                this.BeginAnimation(WidthProperty, animatoin);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ UserControlForRK15/StackMenuItem.xaml.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace UserControlForRK15
+{
+    /// <summary>
+    /// StackMenuItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class StackMenuItem : UserControl
+    {
+
+
+        public Brush HeadTileBackGroup
+        {
+            get { return (Brush)GetValue(HeadTileBackGroupProperty); }
+            set { SetValue(HeadTileBackGroupProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HeadTileBackGroup.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeadTileBackGroupProperty =
+            DependencyProperty.Register("HeadTileBackGroup", typeof(Brush), typeof(StackMenuItem), 
+                new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF02E2FF"))));
+
+
+        public Brush ChildBackGroup
+        {
+            get { return (Brush)GetValue(ChildBackGroupProperty); }
+            set { SetValue(ChildBackGroupProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ChildBackGroup.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ChildBackGroupProperty =
+            DependencyProperty.Register("ChildBackGroup", typeof(Brush), typeof(StackMenuItem),
+                new PropertyMetadata(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF02E2FF"))));
+
+
+
+        public string HeadText
+        {
+            get { return (string)GetValue(HeadTextProperty); }
+            set { SetValue(HeadTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HeadText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeadTextProperty =
+            DependencyProperty.Register("HeadText", typeof(string), typeof(StackMenuItem), new PropertyMetadata("未命名"));
+
+
+
+        public StackMenuItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
