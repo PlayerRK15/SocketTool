54a8116aeb49a0c2ca13baaaf02315d986bab5f4
PlayerRK15 <rk15@qq.com>
2024-04-21 16:19:00 +0800

    style:更新菜单控件

--- UserControlForRK15/StackMenu.xaml.cs
+++ UserControlForRK15/StackMenu.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,7 +23,7 @@ namespace UserControlForRK15
     /// <summary>
     /// UserControl1.xaml 的交互逻辑
     /// </summary>
-    public partial class StackMenu : StackPanel
+    public partial class StackMenu : ItemsControl
     {
         public StackMenu()
         {
@@ -36,7 +37,7 @@ namespace UserControlForRK15
 
         // Using a DependencyProperty as the backing store for IsShow.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty IsShowProperty =
-            DependencyProperty.Register("IsShow", typeof(bool), typeof(StackMenu), new PropertyMetadata(false));
+            DependencyProperty.Register("IsShow", typeof(bool), typeof(StackMenu), new PropertyMetadata(true));
 
 
         public string MenuTiletxt
@@ -50,5 +51,45 @@ namespace UserControlForRK15
             DependencyProperty.Register("MenuTiletxt", typeof(string), typeof(StackMenu), new PropertyMetadata("菜单"));
 
 
+
+        public double MenuTileFontSize
+        {
+            get { return (double)GetValue(MenuTileFontSizeProperty); }
+            set { SetValue(MenuTileFontSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MenuTileFontSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MenuTileFontSizeProperty =
+            DependencyProperty.Register("MenuTileFontSize", typeof(double), typeof(StackMenu), new PropertyMetadata(12d));
+        double? oldWidth;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var txt = (TextBlock)sender;
+            if(IsShow)
+            {
+                IsShow = false;
+                var animator = new DoubleAnimation();
+                var cloumn = Template.FindName("ColumnContent",this) as ColumnDefinition;
+                animator.From = cloumn?.ActualWidth;
+                oldWidth = cloumn?.ActualWidth;
+                animator.To = 0;
+                animator.Duration = TimeSpan.FromSeconds(0.2);
+                cloumn?.BeginAnimation(ColumnDefinition.MaxWidthProperty, animator);
+                (Template.FindName("Show", this) as TextBlock).Text = "▷";
+                (Template.FindName("Close", this) as TextBlock).Text = "";
+            }
+            else
+            {
+                IsShow = true;
+                var animator = new DoubleAnimation();
+                (Template.FindName("Show", this) as TextBlock).Text = "";
+                (Template.FindName("Close", this) as TextBlock).Text = "◁";
+                var cloumn = Template.FindName("ColumnContent", this) as ColumnDefinition;
+                animator.From = 0;
+                animator.To = oldWidth;
+                animator.Duration = TimeSpan.FromSeconds(0.2);
+                cloumn?.BeginAnimation(ColumnDefinition.MaxWidthProperty, animator);
+            }
+        }
     }
 }
--- UserControlForRK15/StackMenuItem.xaml.cs
+++ UserControlForRK15/StackMenuItem.xaml.cs
@@ -10,11 +10,45 @@ namespace UserControlForRK15
     /// <summary>
     /// StackMenuItem.xaml 的交互逻辑
     /// </summary>
-    public partial class StackMenuItem : UserControl
+    public partial class StackMenuItem : ItemsControl
     {
+
+
+        public string TileText
+        {
+            get { return (string)GetValue(TileTextProperty); }
+            set { SetValue(TileTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TileText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TileTextProperty =
+            DependencyProperty.Register("TileText", typeof(string), typeof(StackMenuItem), new PropertyMetadata(""));
+        public Visibility IsShow
+        {
+            get { return (Visibility)GetValue(IsShowProperty); }
+            set { SetValue(IsShowProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsShow.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsShowProperty =
+            DependencyProperty.Register("IsShow", typeof(Visibility), typeof(StackMenuItem), new PropertyMetadata(Visibility.Visible));
+
+
         public StackMenuItem()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsShow == Visibility.Collapsed)
+            {
+                IsShow=Visibility.Visible;
+            }
+            else
+            {
+                IsShow = Visibility.Collapsed;
+            }
         }
     }
 }
